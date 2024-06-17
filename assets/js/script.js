c15bc1e31d6abe390b53d203d78ba9077f1978fc
Ruth de Swart <ruthdeswart@outlook.ie>
2024-06-17 16:29:10 +0000

    Feat: Add functions to display questions and answers in JS File and format file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 //Show Rules Page before Game loads
-window.onload = function() {
-    document.getElementById("rules").style.display="block";
+window.onload = function () {
+    document.getElementById("rules").style.display = "block";
 };
 //Add event listener to click Ok Button and remove rules page
-document.getElementById("rulesOk").addEventListener("click", function() {
+document.getElementById("rulesOk").addEventListener("click", function () {
     document.getElementById("rules").style.display = "none";
 });
 
@@ -19,100 +19,210 @@ const questions = [
     {
         question: "Joey played Dr. Drake Ramoray on which soap opera show?",
         answers: [
-            { text: "Days of our Lives", correct: true},
-            { text: "Knots Landing", correct: false},
-            { text: "Dallas", correct: false},
-            { text: "Dynasty", correct: false}
+            { text: "Days of our Lives", correct: true },
+            { text: "Knots Landing", correct: false },
+            { text: "Dallas", correct: false },
+            { text: "Dynasty", correct: false }
         ]
     },
     {
 
-    question: "How many times did Ross get divorced?",
+        question: "How many times did Ross get divorced?",
         answers: [
-            { text: "Four Times", correct: false},
-            { text: "Three Times", correct: true},
-            { text: "Two", correct: false},
-            { text: "Once", correct: false}
+            { text: "Four Times", correct: false },
+            { text: "Three Times", correct: true },
+            { text: "Two", correct: false },
+            { text: "Once", correct: false }
         ]
-        },
-        {
-            question: "Phoebe's scientist boyfriend David worked in what city?",
-            answers: [
-                { text: "Yemen", correct: false},
-                { text: "Paris", correct: false},
-                { text: "Minsk", correct: true},
-                { text: "London", correct: false}
-            ]
+    },
+    {
+        question: "Phoebe's scientist boyfriend David worked in what city?",
+        answers: [
+            { text: "Yemen", correct: false },
+            { text: "Paris", correct: false },
+            { text: "Minsk", correct: true },
+            { text: "London", correct: false }
+        ]
 
-        },
-        {
+    },
+    {
         question: "What is Chandler Bing's middle name?",
-            answers: [
-                { text: "Muriel", correct: true},
-                { text: "Sue", correct: false},
-                { text: "Joseph", correct: false},
-                { text: "Bong", correct: false}
-            ]
-        },
-        {
-            question: "Brad Pitt and David Schwimmer's characters cofounded what club in high school?",
-                answers: [
-                    { text: "The I Hate Rachel Green Club.", correct: true},
-                    { text: "The Maths Whizz Kidz Club", correct: false},
-                    { text: "The Paleontology Rocks Club", correct: false},
-                    { text: "The Way/No Way Club", correct: false}
-                ]
-            },
-            {
-                question: "Which of Joey's sisters did Chandler fool around with?",
-                    answers: [
-                        { text: "Mary Therese", correct: false},
-                        { text: "Gina", correct: false},
-                        { text: "Mary Angela", correct: true},
-                        { text: "Veronica", correct: false}
-                    ]
-                },
-                {
-                    question: "Monica categorizes her towels into how many categories?",
-                        answers: [
-                            { text: "6", correct: false},
-                            { text: "4", correct: false},
-                            { text: "8", correct: false},
-                            { text: "11", correct: true}
-                        ]
-                    },
-                    {
-                        question: "What form of self-defense does Ross try to teach Rachel and Phoebe?",
-                            answers: [
-                                { text: "Jiu-Jitsu", correct: false},
-                                { text: "Unagi", correct: true},
-                                { text: "Karate", correct: false},
-                                { text: "Taekwondo", correct: false}
-                            ]
-                        },
-                        {
-                            question: "What do Monica and Chandler name their twins?",
-                                answers: [
-                                    { text: "Emma and Ben", correct: false},
-                                    { text: "Chandler and Leslie", correct: false},
-                                    { text: "Erica and Jack", correct: true},
-                                    { text: "Janice and Richard", correct: false}
-                                ]
-                            },
-                            {
-                                question: "Who was Rachel's prom date?",
-                                    answers: [
-                                        { text: "Ross", correct: false},
-                                        { text: "Chip Matthews", correct: true},
-                                        { text: "Tag Jones", correct: false},
-                                        { text: "Barry Farber", correct: false}
-                                    ]
-                                },
+        answers: [
+            { text: "Muriel", correct: true },
+            { text: "Sue", correct: false },
+            { text: "Joseph", correct: false },
+            { text: "Bong", correct: false }
+        ]
+    },
+    {
+        question: "Brad Pitt and David Schwimmer's characters cofounded what club in high school?",
+        answers: [
+            { text: "The I Hate Rachel Green Club.", correct: true },
+            { text: "The Maths Whizz Kidz Club", correct: false },
+            { text: "The Paleontology Rocks Club", correct: false },
+            { text: "The Way/No Way Club", correct: false }
+        ]
+    },
+    {
+        question: "Which of Joey's sisters did Chandler fool around with?",
+        answers: [
+            { text: "Mary Therese", correct: false },
+            { text: "Gina", correct: false },
+            { text: "Mary Angela", correct: true },
+            { text: "Veronica", correct: false }
+        ]
+    },
+    {
+        question: "Monica categorizes her towels into how many categories?",
+        answers: [
+            { text: "6", correct: false },
+            { text: "4", correct: false },
+            { text: "8", correct: false },
+            { text: "11", correct: true }
+        ]
+    },
+    {
+        question: "What form of self-defense does Ross try to teach Rachel and Phoebe?",
+        answers: [
+            { text: "Jiu-Jitsu", correct: false },
+            { text: "Unagi", correct: true },
+            { text: "Karate", correct: false },
+            { text: "Taekwondo", correct: false }
+        ]
+    },
+    {
+        question: "What do Monica and Chandler name their twins?",
+        answers: [
+            { text: "Emma and Ben", correct: false },
+            { text: "Chandler and Leslie", correct: false },
+            { text: "Erica and Jack", correct: true },
+            { text: "Janice and Richard", correct: false }
+        ]
+    },
+    {
+        question: "Who was Rachel's prom date?",
+        answers: [
+            { text: "Ross", correct: false },
+            { text: "Chip Matthews", correct: true },
+            { text: "Tag Jones", correct: false },
+            { text: "Barry Farber", correct: false }
+        ]
+    },
 
 ];
 
-let currentQuestion = document.getElementById("question");
-let currentAnswer = document.getElementsByClassName("answer-box");
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementsByClassName("ans-btn");
+const submitButton = document.getElementsByClassName("next-btn");
+
+let currentQuestion = 0;
 let score = 0;
-let nextQuestion = document.getElementsByClassName("next-btn");
-let currentQuestionIndex = 0;
+
+
+function showQuestion() {
+
+    const question = questions[currentQuestion];
+    questionElement.innerText = currentQuestion.questions;
+
+
+currentQuestion.answers.forEach(answer => {
+  const button = document.createElement("button");
+  button.innerHTML = answer.text;
+  button.classList.add("ans-btn");
+  answerBox.appendChild(button);
+  button.addEventListener("click", selectAnswer);
+});
+}
+
+function selectAnswer(event) {
+    const answerElement = event.target;
+    const answer = questions[questionElement].answers;
+  
+    if (answers.correct === true) {
+      score++;
+    }
+  
+    currentQuestion++;
+  
+    if (currentQuestion < questions.length) {
+      showQuestion();
+    } else {
+      showResult();
+    }
+}
+  
+  
+  function showResult() {
+    //questionContainer??
+    question-container.innerHTML ; `  
+      <h1>Quiz Completed!</h1>
+      <p>Your score: ${score}/${questions.length}</p>
+    `;
+  }
+  
+  showQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//Function to display Questions
+/*function askQuestion() {
+    for (var i =0; i < questions.length; i++) {
+        var question = questions[i].question;
+        var answer = questions[i].answers;
+
+        var userAns = ();
+
+        if (userAns === true) {
+            alert("Well done!");
+        } else {
+            alert("Nope, that's wrong!")
+        }
+    }
+}
+*/
+
+/*function showQuestion() {
+    const questionText = document.getElementById("question");
+    questionText.textContent = questions[currentQuestion].question;
+
+    const answers = document.querySelectorAll(".ans-btn");
+    answers.forEach((answers, index) => {
+        answer.textContent = questions[currentQuestion].answers[index];
+    });
+
+    /*if (userAns === true) {
+        alert("Well done!");
+    } else {
+        alert("Nope, that's wrong!")
+    }*/
+
+
+//Function to calculate score
+
+//askQuestion();
